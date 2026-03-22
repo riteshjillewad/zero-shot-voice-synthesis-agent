adc8cb1474efe5baf28c09ce355583c2ed757e07
Ritesh Jillewad <130166456+riteshjillewad@users.noreply.github.com>
2026-03-22 16:55:24 +0530

    Update tts_model.py

--- models/tts_model.py
+++ models/tts_model.py
@@ -1,1 +1,10 @@
+# models/tts_model.py
 
+from TTS.api import TTS
+from config import MODEL_NAME
+
+# Load model globally (so it doesn't reload every time)
+tts_model = TTS(model_name=MODEL_NAME)
+
+def get_tts_model():
+    return tts_model
